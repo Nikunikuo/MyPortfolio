9f96a2435cc64f8a2c144765172a3c4af59b2151
Nikunikuo <yajusika@gmail.com>
2026-04-01 10:02:40 +0900

    Replace top page videos with YouTube and remove huge video files

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,4 +6,9 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://niku.studio',
 	integrations: [sitemap()],
+	vite: {
+		resolve: {
+			preserveSymlinks: true,
+		},
+	},
 });
